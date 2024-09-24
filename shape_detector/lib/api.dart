a8d49f6ea3f434a15986cccf44b228a3da88b8ec
Mohammed Nasim <mohammednasim2004@gmail.com>
2024-09-24 13:05:46 +0530

    final

--- shape_detector/lib/api.dart
+++ shape_detector/lib/api.dart
@@ -1,90 +1,9 @@
-// import 'dart:io';
-// import 'dart:typed_data';
-// import 'package:http/http.dart' as http;
-// import 'package:path_provider/path_provider.dart'; // For saving the STL file
-
-// Future<void> sendImageAndReceiveSTL(String imagePath) async {
-//   final url = Uri.parse('https://lascade-task.onrender.com/process-image/');
-
-//   try {
-//     // Create the multipart request
-//     var request = http.MultipartRequest('POST', url);
-
-//     // Add the image file using the image path
-//     request.files.add(await http.MultipartFile.fromPath(
-//       'file',
-//       imagePath,
-//       filename: 'image.png', // You can set a custom filename if needed
-//     ));
-
-//     // Send the request
-//     var response = await request.send();
-
-//     // Check for success
-//     if (response.statusCode == 200) {
-//       // Get the response as bytes
-//       Uint8List bytes = await response.stream.toBytes();
-
-//       // Save the STL file to local storage
-//       final directory = await getApplicationDocumentsDirectory();
-//       final filePath = '${directory.path}/filename.stl';
-//       File stlFile = File(filePath);
-//       await stlFile.writeAsBytes(bytes);
-
-//       print('STL file saved at: $filePath');
-//     } else {
-//       print(
-//           'Error: Failed to get the STL file. Status code: ${response.statusCode}');
-//     }
-//   } catch (e) {
-//     print('Error: $e');
-//   }
-// }
-
-// import 'dart:io';
-// import 'dart:typed_data';
-// import 'package:http/http.dart' as http;
-// import 'package:path_provider/path_provider.dart';
-
-// Future<String?> sendImageAndReceiveSTL(String imagePath) async {
-//   final url = Uri.parse('https://lascade-task.onrender.com/process-image/');
-
-//   try {
-//     var request = http.MultipartRequest('POST', url);
-//     request.files.add(await http.MultipartFile.fromPath(
-//       'file',
-//       imagePath,
-//       filename: 'image.png',
-//     ));
-
-//     var response = await request.send();
-
-//     if (response.statusCode == 200) {
-//       Uint8List bytes = await response.stream.toBytes();
-//       final directory = await getApplicationDocumentsDirectory();
-//       final filePath = '${directory.path}/received_model.glb';
-//       File stlFile = File(filePath);
-//       await stlFile.writeAsBytes(bytes);
-
-//       print('STL file saved at: $filePath');
-//       return filePath; // Return the path to the saved STL file
-//     } else {
-//       print(
-//           'Error: Failed to get the STL file. Status code: ${response.statusCode}');
-//       return null;
-//     }
-//   } catch (e) {
-//     print('Error: $e');
-//     return null;
-//   }
-// }
-//
 import 'dart:io';
 import 'dart:typed_data';
 import 'package:http/http.dart' as http;
 import 'package:path_provider/path_provider.dart';
 
-Future<String?> sendImageAndReceivePLB(String imagePath) async {
+Future<String?> sendImageAndReceiveGLB(String imagePath) async {
   final url = Uri.parse('https://lascade-task.onrender.com/process-image/');
 
   try {
@@ -101,15 +20,15 @@ Future<String?> sendImageAndReceivePLB(String imagePath) async {
       Uint8List bytes = await response.stream.toBytes();
       final directory = await getApplicationDocumentsDirectory();
       final filePath =
-          '${directory.path}/received_model.plb'; // Change extension to .plb
-      File plbFile = File(filePath);
-      await plbFile.writeAsBytes(bytes);
+          '${directory.path}/received_model.glb'; // Change extension to .glb
+      File glbFile = File(filePath);
+      await glbFile.writeAsBytes(bytes);
 
-      print('PLB file saved at: $filePath');
-      return filePath; // Return the path to the saved PLB file
+      print('GLB file saved at: $filePath');
+      return filePath; // Return the path to the saved GLB file
     } else {
       print(
-          'Error: Failed to get the PLB file. Status code: ${response.statusCode}');
+          'Error: Failed to get the GLB file. Status code: ${response.statusCode}');
       return null;
     }
   } catch (e) {
--- shape_detector/lib/homepage.dart
+++ shape_detector/lib/homepage.dart
@@ -1,376 +1,3 @@
-// // import 'package:camera/camera.dart';
-// // import 'package:flutter/material.dart';
-// // import 'package:gal/gal.dart';
-// // import 'package:shape_detector/api.dart';
-// // import 'package:shape_detector/ar.dart';
-
-// // class HomePage extends StatefulWidget {
-// //   const HomePage({super.key});
-
-// //   @override
-// //   State<HomePage> createState() => _HomePageState();
-// // }
-
-// // class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
-// //   List<CameraDescription> cameras = [];
-// //   CameraController? cameraController;
-
-// //   @override
-// //   void didChangeAppLifecycleState(AppLifecycleState state) {
-// //     super.didChangeAppLifecycleState(state);
-// //     if (cameraController == null ||
-// //         cameraController?.value.isInitialized == false) {
-// //       return;
-// //     }
-
-// //     if (state == AppLifecycleState.inactive) {
-// //       cameraController?.dispose();
-// //     } else if (state == AppLifecycleState.resumed) {
-// //       _setupCameraController();
-// //     }
-// //   }
-
-// //   @override
-// //   void initState() {
-// //     super.initState();
-// //     _setupCameraController();
-// //   }
-
-// //   @override
-// //   Widget build(BuildContext context) {
-// //     return Scaffold(
-// //       body: _buildUI(),
-// //     );
-// //   }
-
-// //   Widget _buildUI() {
-// //     if (cameraController == null ||
-// //         cameraController?.value.isInitialized == false) {
-// //       return const Center(
-// //         child: CircularProgressIndicator(),
-// //       );
-// //     }
-// //     return SafeArea(
-// //       child: SizedBox.expand(
-// //         child: Column(
-// //           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-// //           crossAxisAlignment: CrossAxisAlignment.center,
-// //           children: [
-// //             SizedBox(
-// //               height: MediaQuery.sizeOf(context).height * 0.80,
-// //               width: MediaQuery.sizeOf(context).width * 0.80,
-// //               child: CameraPreview(
-// //                 cameraController!,
-// //               ),
-// //             ),
-// //             IconButton(
-// //               onPressed: () async {
-// //                 XFile picture = await cameraController!.takePicture();
-// //                 String imagePath = picture.path;
-// //                 String? stlFilePath = await sendImageAndReceiveSTL(imagePath);
-// //                 // Gal.putImage(
-// //                 //   picture.path,
-// //                 // );
-
-// //                 if (stlFilePath != null) {
-// //                   // Now you can use this path to display the STL file in your AR view
-// //                   print(stlFilePath);
-// //                   Navigator.push(
-// //                     context,
-// //                     MaterialPageRoute(
-// //                       builder: (context) => ARView(
-// //                           usdzFilePath: stlFilePath), // Use the STL path here
-// //                     ),
-// //                   );
-// //                 } else {
-// //                   print('Failed to receive STL file.');
-// //                 }
-// //               },
-// //               iconSize: 100,
-// //               icon: const Icon(
-// //                 Icons.camera,
-// //                 color: Colors.red,
-// //               ),
-// //             )
-// //           ],
-// //         ),
-// //       ),
-// //     );
-// //   }
-
-// //   Future<void> _setupCameraController() async {
-// //     List<CameraDescription> _cameras = await availableCameras();
-// //     if (_cameras.isNotEmpty) {
-// //       setState(() {
-// //         cameras = _cameras;
-// //         cameraController = CameraController(
-// //           _cameras.first,
-// //           ResolutionPreset.high,
-// //         );
-// //       });
-// //       cameraController?.initialize().then((_) {
-// //         if (!mounted) {
-// //           return;
-// //         }
-// //         setState(() {});
-// //       }).catchError(
-// //         (Object e) {
-// //           print(e);
-// //         },
-// //       );
-// //     }
-// //   }
-// // }
-
-// import 'package:camera/camera.dart';
-// import 'package:flutter/material.dart';
-// import 'package:shape_detector/api.dart';
-// import 'package:shape_detector/ar.dart';
-
-// class HomePage extends StatefulWidget {
-//   const HomePage({super.key});
-
-//   @override
-//   State<HomePage> createState() => _HomePageState();
-// }
-
-// class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
-//   List<CameraDescription> cameras = [];
-//   CameraController? cameraController;
-
-//   @override
-//   void didChangeAppLifecycleState(AppLifecycleState state) {
-//     super.didChangeAppLifecycleState(state);
-//     if (cameraController == null ||
-//         cameraController?.value.isInitialized == false) {
-//       return;
-//     }
-
-//     if (state == AppLifecycleState.inactive) {
-//       cameraController?.dispose();
-//     } else if (state == AppLifecycleState.resumed) {
-//       _setupCameraController();
-//     }
-//   }
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     _setupCameraController();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: _buildUI(),
-//     );
-//   }
-
-//   Widget _buildUI() {
-//     if (cameraController == null ||
-//         cameraController?.value.isInitialized == false) {
-//       return const Center(
-//         child: CircularProgressIndicator(),
-//       );
-//     }
-//     return SafeArea(
-//       child: SizedBox.expand(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//           crossAxisAlignment: CrossAxisAlignment.center,
-//           children: [
-//             SizedBox(
-//               height: MediaQuery.sizeOf(context).height * 0.80,
-//               width: MediaQuery.sizeOf(context).width * 0.80,
-//               child: CameraPreview(
-//                 cameraController!,
-//               ),
-//             ),
-//             IconButton(
-//               onPressed: () async {
-//                 print('Camera button pressed'); // Debug print
-//                 XFile picture = await cameraController!.takePicture();
-//                 String imagePath = picture.path;
-//                 print('Picture taken at: $imagePath'); // Debug print
-//                 String? glbFilePath = await sendImageAndReceiveSTL(imagePath);
-//                 print('GLB file path received: $glbFilePath'); // Debug print
-
-//                 if (glbFilePath != null && glbFilePath.isNotEmpty) {
-//                   print(
-//                       'Displaying GLB file with path: $glbFilePath'); // Debug print
-//                   setState(() {
-//                     _modelPath = glbFilePath; // Store the path for rendering
-//                   });
-//                 } else {
-//                   ScaffoldMessenger.of(context).showSnackBar(
-//                     SnackBar(content: Text('Failed to receive GLB file.')),
-//                   );
-//                 }
-//               },
-//               iconSize: 100,
-//               icon: const Icon(
-//                 Icons.camera,
-//                 color: Colors.red,
-//               ),
-//             )
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-
-//   Future<void> _setupCameraController() async {
-//     List<CameraDescription> _cameras = await availableCameras();
-//     if (_cameras.isNotEmpty) {
-//       setState(() {
-//         cameras = _cameras;
-//         cameraController = CameraController(
-//           _cameras.first,
-//           ResolutionPreset.high,
-//         );
-//       });
-//       cameraController?.initialize().then((_) {
-//         if (!mounted) {
-//           return;
-//         }
-//         setState(() {});
-//       }).catchError(
-//         (Object e) {
-//           print(e);
-//         },
-//       );
-//     }
-//   }
-// }
-
-// import 'package:camera/camera.dart';
-// import 'package:flutter/material.dart';
-// import 'package:shape_detector/api.dart';
-// import 'package:shape_detector/ar.dart'; // Ensure this imports your GLB model viewer
-
-// class HomePage extends StatefulWidget {
-//   const HomePage({super.key});
-
-//   @override
-//   State<HomePage> createState() => _HomePageState();
-// }
-
-// class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
-//   List<CameraDescription> cameras = [];
-//   CameraController? cameraController;
-//   String? _modelPath; // Variable to hold the model path
-
-//   @override
-//   void didChangeAppLifecycleState(AppLifecycleState state) {
-//     super.didChangeAppLifecycleState(state);
-//     if (cameraController == null ||
-//         cameraController?.value.isInitialized == false) {
-//       return;
-//     }
-
-//     if (state == AppLifecycleState.inactive) {
-//       cameraController?.dispose();
-//     } else if (state == AppLifecycleState.resumed) {
-//       _setupCameraController();
-//     }
-//   }
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     _setupCameraController();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: _buildUI(),
-//     );
-//   }
-
-//   Widget _buildUI() {
-//     if (cameraController == null ||
-//         cameraController?.value.isInitialized == false) {
-//       return const Center(
-//         child: CircularProgressIndicator(),
-//       );
-//     }
-
-//     return SafeArea(
-//       child: SizedBox.expand(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//           crossAxisAlignment: CrossAxisAlignment.center,
-//           children: [
-//             SizedBox(
-//               height: MediaQuery.sizeOf(context).height * 0.80,
-//               width: MediaQuery.sizeOf(context).width * 0.80,
-//               child: CameraPreview(cameraController!),
-//             ),
-//             IconButton(
-//               onPressed: () async {
-//                 print('Camera button pressed'); // Debug print
-//                 XFile picture = await cameraController!.takePicture();
-//                 String imagePath = picture.path;
-//                 print('Picture taken at: $imagePath'); // Debug print
-//                 String? glbFilePath = await sendImageAndReceiveSTL(imagePath);
-//                 print('GLB file path received: $glbFilePath'); // Debug print
-
-//                 if (glbFilePath != null && glbFilePath.isNotEmpty) {
-//                   print(
-//                       'Displaying GLB file with path: $glbFilePath'); // Debug print
-//                   setState(() {
-//                     _modelPath = glbFilePath; // Store the path for rendering
-//                   });
-//                 } else {
-//                   ScaffoldMessenger.of(context).showSnackBar(
-//                     SnackBar(content: Text('Failed to receive GLB file.')),
-//                   );
-//                 }
-//               },
-//               iconSize: 100,
-//               icon: const Icon(
-//                 Icons.camera,
-//                 color: Colors.red,
-//               ),
-//             ),
-//             // Display the model if a path is set
-//             if (_modelPath != null)
-//               Expanded(
-//                 child: YourModelViewerWidget(
-//                     modelPath:
-//                         _modelPath!), // Replace with your model viewer widget
-//               ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-
-//   Future<void> _setupCameraController() async {
-//     List<CameraDescription> _cameras = await availableCameras();
-//     if (_cameras.isNotEmpty) {
-//       setState(() {
-//         cameras = _cameras;
-//         cameraController = CameraController(
-//           _cameras.first,
-//           ResolutionPreset.high,
-//         );
-//       });
-//       cameraController?.initialize().then((_) {
-//         if (!mounted) {
-//           return;
-//         }
-//         setState(() {});
-//       }).catchError(
-//         (Object e) {
-//           print(e);
-//         },
-//       );
-//     }
-//   }
-// }
-//
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:shape_detector/api.dart';
@@ -390,7 +17,8 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
   @override
   void didChangeAppLifecycleState(AppLifecycleState state) {
     super.didChangeAppLifecycleState(state);
-    if (cameraController == null || cameraController?.value.isInitialized == false) {
+    if (cameraController == null ||
+        cameraController?.value.isInitialized == false) {
       return;
     }
 
@@ -415,7 +43,8 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
   }
 
   Widget _buildUI() {
-    if (cameraController == null || cameraController?.value.isInitialized == false) {
+    if (cameraController == null ||
+        cameraController?.value.isInitialized == false) {
       return const Center(
         child: CircularProgressIndicator(),
       );
@@ -438,18 +67,19 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
                 XFile picture = await cameraController!.takePicture();
                 String imagePath = picture.path;
                 print('Picture taken at: $imagePath'); // Debug print
-                String? plbFilePath = await sendImageAndReceivePLB(imagePath); // Use the updated function
+                String? glbFilePath = await sendImageAndReceiveGLB(imagePath);
 
-                print('PLB file path received: $plbFilePath'); // Debug print
+                print('GLB file path received: $glbFilePath'); // Debug print
 
-                if (plbFilePath != null && plbFilePath.isNotEmpty) {
-                  print('Displaying PLB file with path: $plbFilePath'); // Debug print
+                if (glbFilePath != null && glbFilePath.isNotEmpty) {
+                  print(
+                      'Displaying GLB file with path: $glbFilePath'); // Debug print
                   setState(() {
-                    _modelPath = plbFilePath; // Store the path for displaying
+                    _modelPath = glbFilePath; // Store the path for displaying
                   });
                 } else {
                   ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('Failed to receive PLB file.')),
+                    SnackBar(content: Text('Failed to receive GLB file.')),
                   );
                 }
               },
@@ -464,7 +94,7 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
               Padding(
                 padding: const EdgeInsets.all(16.0),
                 child: Text(
-                  'PLB File Path:\n$_modelPath', // Updated to PLB
+                  'GLB File Path:\n$_modelPath', // Updated to GLB
                   textAlign: TextAlign.center,
                   style: TextStyle(fontSize: 16, color: Colors.black),
                 ),
